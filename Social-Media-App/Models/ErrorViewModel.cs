a1dbd692a75c5d7e4990afae9d16b9b323b277b0
Mladen Nedev <mlados43@gmail.com>
2023-06-14 00:41:55 +0300

    Initial commit

---
+++ Social-Media-App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Social_Media_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
