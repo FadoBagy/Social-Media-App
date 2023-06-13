85082723736689c36c6423c6bf62eba6dd5305fd
Mladen Nedev <mlados43@gmail.com>
2023-06-14 02:27:15 +0300

    Test

--- Social-Media-App/Program.cs
+++ Social-Media-App/Program.cs
@@ -20,7 +20,6 @@ namespace Social_Media_App
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
@@ -28,7 +27,6 @@ namespace Social_Media_App
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
