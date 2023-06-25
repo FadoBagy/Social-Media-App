a11088f7cf6b3bf159fd43cd1efbba74e0aefa42
Mladen Nedev <mlados43@gmail.com>
2023-06-26 01:07:37 +0300

    Adding Authorized Attributes to Controllers

--- src/Social-Media-App/Controllers/ChatController.cs
+++ src/Social-Media-App/Controllers/ChatController.cs
@@ -1,7 +1,9 @@
 ﻿namespace Social_Media_App.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
+    [Authorize]
     public class ChatController : Controller
     {
         public IActionResult Index()
